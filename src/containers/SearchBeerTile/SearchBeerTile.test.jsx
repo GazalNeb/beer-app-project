65b8c272fbbcd6857edea4aa5c98906a73678a10
gazalneb <nebgazal@gmail.com>
2022-04-28 14:31:33 +0100

    SearchBeerTile test works

--- src/containers/SearchBeerTile/SearchBeerTile.test.jsx
+++ src/containers/SearchBeerTile/SearchBeerTile.test.jsx
@@ -2,12 +2,13 @@ import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CardList from "../../components/CardList/CardList";
 import SearchBeerTile from "./SearchBeerTile";
+import beers from "../../data/beers";
 
 it("Should render the total number of beers on screen", () => {
   //1. Arrange
-  render(<SearchBeerTile />);
+  render(<SearchBeerTile beersArr={beers}/>);
   //2. Act
-  const searchCountText = screen.getByText("You see 80 types of beer");
+  const searchCountText = screen.getByText(/You see .* types of beer/i);
   //3. Assert
   expect(searchCountText).toBeInTheDocument();
 });
