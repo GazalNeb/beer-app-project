53ef6936d5b8cff9753d999a4d4ff0c821e68baa
gazalneb <nebgazal@gmail.com>
2022-05-03 17:02:42 +0100

    some more styling changes in card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,11 +7,11 @@ const Card = ({image, name, description, first_brewed, abv, ph}) => {
     <div className='card'>
      <img className='card__image'src={image}/>
      <h2 className='card__name'>{name}</h2>
-     <p className='card__description'>{description}</p>
+     <p className='card__description'><i>{description}</i></p>
      <div className="card__brewers">
-       <p className='card__brewers__year'><i>Year it was first brewed: {first_brewed}</i></p>
-       <p className='card__brewers__abv'><i>Abv value: {abv}</i></p>
-       <p className='card__brewers__ph'><i>pH value: {ph}</i></p>
+       <p className='card__brewers__year'><i><b>Year it was first brewed:</b> {first_brewed}</i></p>
+       <p className='card__brewers__abv'><i><b>Abv value:</b> {abv}</i></p>
+       <p className='card__brewers__ph'><i><b>pH value: {ph}</b></i></p>
      </div>
     </div>
   )
